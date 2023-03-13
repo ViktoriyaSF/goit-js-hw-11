240b483ca5dc5f98a04cceeaef0dc69f06ca42a9
ViktoriyaSF <kievstar01051985@gmail.com>
2023-03-13 23:00:04 +0200

    part1

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ const url = 'https://pixabay.com/api/';
 const KEY = '34347073-8f1b60398676bada9d735fc2f';
 const restAPI = '&image_type=photo&orientation=horizontal&safesearch=true';
 
-function searchPhoto(namePhoto) {
-  return fetch(`${url}?key=${KEY}&q=${namePhoto}${restAPI}`).then(res => {
-    //     .then(object => {
-    // if (object.totalHits === 0) {
-    //   console.log(
-    //     'Sorry, there are no images matching your search query. Please try again'
-    //   );
-    // }
+function searchPhoto(namePhoto, page, perPage = 40) {
+  return fetch(
+    `${url}?key=${KEY}&q=${namePhoto}${restAPI}&page=${page}&per_page=${perPage}`
+  ).then(res => {
     return res.json();
   });
 }
@@ -18,26 +14,22 @@ function searchPhoto(namePhoto) {
 const searchFormPoto = document.querySelector('#search-form');
 const galleryPhoto = document.querySelector('.gallery');
 
-// webformatURL - посилання на маленьке зображення для списку obкарток.scr
-// largeImageURL - посилання на велике зображення.href
-// tags - рядок з описом зображення. Підійде для атрибуту alt.
-// likes - кількість лайків.
-// views - кількість переглядів.
-// comments - кількість коментарів.
-// downloads - кількість завантажень.
-
 searchFormPoto.addEventListener('submit', onSubmitPhoto);
-function onSubmitPhoto(e) {
+async function onSubmitPhoto(e) {
   e.preventDefault();
   const queryPhoto = e.target.elements.searchQuery.value.trim();
-  console.log(searchPhoto(queryPhoto));
+  if (!queryPhoto) {
+    return;
+  }
+  const data = await searchPhoto(queryPhoto); // значення що сформоване
+  cardPhoto(data);
 }
 
-// функція для свторення картки
+// функція для створення картки вибираючи по hits
 function cardPhoto(arr) {
   const markUp = arr.hits
     .map(el => {
-      `<div class="photo-card"><a a class="gallery-link" href="${el.largeImageURL}"><img src="${el.webformatURL}" alt="${el.tags}" loading="lazy" />
+      return `<div class="photo-card"><a a class="gallery-link" href="${el.largeImageURL}"><img src="${el.webformatURL}" alt="${el.tags}" loading="lazy" />
         </a>
     <div class="info">
       <p class="info-item">
